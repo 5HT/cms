d20eb9e2e589509c9a0fe95d5d04adaadfd20bde
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-11-15 02:19:49 +0200

    include all.min.js

--- javascript.sh
+++ javascript.sh
@@ -17,6 +17,6 @@ function create_script {
 create_script apps/web/priv/static/bootstrap/js apps/web/priv/static/js/bootstrap.min.js "/"
 create_script apps/web/priv/static/n2o_bt/js apps/web/priv/static/js/n2o-bootstrap.min.js "/"
 create_script deps/n2o/priv/static/n2o apps/web/priv/static/js/all.min.js "/|all.js|zepto.js"
-create_script apps/web/priv/static/js apps/web/priv/static/js/countach.min.js "/|countach.min.js|all.min.js"
+create_script apps/web/priv/static/js apps/web/priv/static/js/countach.min.js "/|countach.min.js"
 
 echo $?
